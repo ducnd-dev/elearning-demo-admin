fe94ebb8827af67aebe18129d63ad9519963ad89
huongtx <phamhyta@gmail.com>
2024-10-20 00:10:04 +0700

    Refactor ModalCourseMaterial component to add loading state during file upload

--- src/pages/Category/ModalCourseMaterial.jsx
+++ src/pages/Category/ModalCourseMaterial.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Drawer, Form, Button, Input, Upload, Checkbox, Image, Spin } from "antd";
-import apiUpload from '../../api/upload';
+import { Drawer, Form, Button, Input, Upload, Checkbox, Image, Spin, Progress } from "antd";
+// import apiUpload from '../../api/upload';
 import { getImageUrl } from '../../common';
+import axios from 'axios';
+import { useAuth } from '../../hooks/useAuth';
+import { baseURL } from '../../constants/api';
 
 const initialValues = {
     title: '',
@@ -19,6 +22,8 @@ const ModalCourseMaterial = ({ show, handleClose, handleSubmit, formData = initi
     const [image, setImage] = useState(formData.img);
     const [video, setVideo] = useState(formData.file_path);
     const [loading, setLoading] = useState(false);
+    const [progress, setProgress] = useState();
+    const { token } = useAuth();
 
     useEffect(() => {
         form.resetFields();
@@ -32,12 +37,22 @@ const ModalCourseMaterial = ({ show, handleClose, handleSubmit, formData = initi
         try {
             const formData = new FormData();
             formData.append('file', file);
-            const res = await apiUpload.uploadFile(formData);
-            return res.path;
+            // const res = await apiUpload.uploadFile(formData);
+            const res = await axios.post(`${baseURL}/api/v1/admin/upload`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${token?.replace(/['"]+/g, '')}`
+                },
+                onUploadProgress: (progressEvent) => {
+                    setProgress(Math.round(progressEvent.loaded * 100 / progressEvent.total) - 1);
+                }
+            });
+            return res.data.path;
         } catch (error) {
             console.log(error);
         } finally {
             setLoading(false);
+            setProgress(0);
         }
     }
 
@@ -48,118 +63,117 @@ const ModalCourseMaterial = ({ show, handleClose, handleSubmit, formData = initi
             onClose={handleClose}
             width={600}
         >
-            <Spin spinning={loading}>
-                <Form
-                    initialValues={formData}
-                    form={form}
-                    layout="vertical"
-                    onFinish={(values) => {
-                        handleSubmit(values)
-                        form.resetFields();
-                    }}
-                    autoComplete="off"
+            <Form
+                initialValues={formData}
+                form={form}
+                layout="vertical"
+                onFinish={(values) => {
+                    handleSubmit(values)
+                    form.resetFields();
+                }}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Tiêu đề"
+                    name="title"
+                    rules={[{ required: true, message: 'Vui lòng nhập tiêu đề!' }]}
                 >
-                    <Form.Item
-                        label="Tiêu đề"
-                        name="title"
-                        rules={[{ required: true, message: 'Vui lòng nhập tiêu đề!' }]}
-                    >
-                        <Input placeholder='Nhập tiêu đề' />
-                    </Form.Item>
-                    <Form.Item
-                        label="Miêu tả"
-                        name="description"
-                        rules={[{ required: true, message: 'Vui lòng nhập miêu tả!' }]}
-                    >
-                        <Input.TextArea placeholder='Nhập miêu tả' />
-                    </Form.Item>
+                    <Input placeholder='Nhập tiêu đề' />
+                </Form.Item>
+                <Form.Item
+                    label="Miêu tả"
+                    name="description"
+                    rules={[{ required: true, message: 'Vui lòng nhập miêu tả!' }]}
+                >
+                    <Input.TextArea placeholder='Nhập miêu tả' />
+                </Form.Item>
 
-                    <Form.Item
-                        label="Thời lượng"
-                        name="time"
-                        rules={[{ required: true, message: 'Vui lòng nhập thời lượng!' }]}
-                    >
-                        <Input placeholder='Nhập thời lượng' />
-                    </Form.Item>
+                <Form.Item
+                    label="Thời lượng"
+                    name="time"
+                    rules={[{ required: true, message: 'Vui lòng nhập thời lượng!' }]}
+                >
+                    <Input placeholder='Nhập thời lượng' />
+                </Form.Item>
 
-                    <Form.Item label="Hình ảnh" name='img'>
-                        <div className='flex'>
-                            <Upload
-                                showUploadList={false}
-                                action={async (file) => {
-                                    const res = await handleUpload(file);
-                                    form.setFieldsValue({ img: res });
-                                    setImage(res);
-                                }}
-                                accept='image/*'
-                            >
-                                <Button className='w-40 h-40 border-dashed rounded-xl'>
-                                    <div className='text-center'>+ Click để tải lên <br /> hình ảnh</div>
-                                </Button>
-                            </Upload>
-                            <div className='ml-4 h-40'>
-                                {image && <Image src={getImageUrl(image)} alt="img" height={160} />}
-                            </div>
+                <Form.Item label="Hình ảnh" name='img'>
+                    <div className='flex'>
+                        <Upload
+                            showUploadList={false}
+                            action={async (file) => {
+                                const res = await handleUpload(file);
+                                form.setFieldsValue({ img: res });
+                                setImage(res);
+                            }}
+                            accept='image/*'
+                        >
+                            <Button className='w-40 h-40 border-dashed rounded-xl'>
+                                <div className='text-center'>+ Click để tải lên <br /> hình ảnh</div>
+                            </Button>
+                        </Upload>
+                        <div className='ml-4 h-40'>
+                            {image && <Image src={getImageUrl(image)} alt="img" height={160} />}
                         </div>
-                    </Form.Item>
-                    
-                    <Form.Item label="Video" name='file_path'>
-                        <div className='flex'>
-                            <Upload
-                                showUploadList={false}
-                                beforeUpload={async (file) => {
-                                    const res = await handleUpload(file);
-                                    form.setFieldsValue({ file_path: res });
-                                    setVideo(res);
-                                }}
-                                accept='video/*'
-                            >
-                                <Button className='w-40 h-40 boder border-dashed rounded-xl'>
-                                    <div className='text-center'>+ Click để tải lên <br /> video</div>
-                                </Button>
-                            </Upload>
-                            <div className='ml-4 h-40'>
-                                {video && <video src={getImageUrl(video)} controls className="aspect-video h-40"/>}
-                            </div>
-                        </div>
-                    </Form.Item>
-
-                    <div className='grid md:grid-cols-3 gap-4'>
-                        <Form.Item name="is_free">
-                            <Checkbox
-                                defaultChecked={ formData.is_free === 1 }
-                                onChange={(e) => form.setFieldsValue({ is_free: e.target.checked ? 1 : 0 })}
-                            >
-                                Miễn phí
-                            </Checkbox>
-                        </Form.Item>
-
-                        <Form.Item name="is_important">
-                            <Checkbox
-                                defaultChecked={formData.is_important === 1}
-                                onChange={(e) => form.setFieldsValue({ is_important: e.target.checked ? 1 : 0 })}
-                            >
-                                Quan trọng
-                            </Checkbox>
-                        </Form.Item>
-
-                        <Form.Item name="is_featured">
-                            <Checkbox
-                                defaultChecked={formData.is_featured === 1}
-                                onChange={(e) => form.setFieldsValue({ is_featured: e.target.checked ? 1 : 0 })}
-                            >
-                                Nổi bật
-                            </Checkbox>
-                        </Form.Item>
                     </div>
+                </Form.Item>
+                
+                <Form.Item label="Video" name='file_path'>
+                    <div className='flex'>
+                        <Upload
+                            showUploadList={false}
+                            beforeUpload={async (file) => {
+                                const res = await handleUpload(file);
+                                form.setFieldsValue({ file_path: res });
+                                setVideo(res);
+                            }}
+                            accept='video/*'
+                        >
+                            <Button className='w-40 h-40 boder border-dashed rounded-xl'>
+                                <div className='text-center'>+ Click để tải lên <br /> video</div>
+                            </Button>
+                        </Upload>
+                        <div className='ml-4 h-40'>
+                            {video && <video src={getImageUrl(video)} controls className="aspect-video h-40"/>}
+                        </div>
+                    </div>
+                </Form.Item>
+                {progress && loading && <Progress percent={progress} />}
 
-                    <Form.Item className="mt-8 flex justify-center">
-                        <Button type="primary" htmlType="submit" className="w-48">
-                            Lưu
-                        </Button>
+                <div className='grid md:grid-cols-3 gap-4'>
+                    <Form.Item name="is_free">
+                        <Checkbox
+                            defaultChecked={ formData.is_free === 1 }
+                            onChange={(e) => form.setFieldsValue({ is_free: e.target.checked ? 1 : 0 })}
+                        >
+                            Miễn phí
+                        </Checkbox>
                     </Form.Item>
-                </Form>
-            </Spin>
+
+                    <Form.Item name="is_important">
+                        <Checkbox
+                            defaultChecked={formData.is_important === 1}
+                            onChange={(e) => form.setFieldsValue({ is_important: e.target.checked ? 1 : 0 })}
+                        >
+                            Quan trọng
+                        </Checkbox>
+                    </Form.Item>
+
+                    <Form.Item name="is_featured">
+                        <Checkbox
+                            defaultChecked={formData.is_featured === 1}
+                            onChange={(e) => form.setFieldsValue({ is_featured: e.target.checked ? 1 : 0 })}
+                        >
+                            Nổi bật
+                        </Checkbox>
+                    </Form.Item>
+                </div>
+
+                <Form.Item className="mt-8 flex justify-center">
+                    <Button type="primary" htmlType="submit" className="w-48">
+                        Lưu
+                    </Button>
+                </Form.Item>
+            </Form>
         </Drawer>
     );
 }
